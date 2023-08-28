fcf0b821bdaeeff34ba1791722c656b389a35563
alexxmatei <49484290+alexxmatei@users.noreply.github.com>
2023-08-28 19:17:25 +0300

    Create nvim-config.init.lua

---
+++ IDEs/nvim-config.init.lua
@@ -0,0 +1,3 @@
+-- Set a vertical ruler as a recommended best practice not to cross over
+-- This makes it easier to work with multiple editors in split windows amongst other things
+vim.opt.colorcolumn = "80"
