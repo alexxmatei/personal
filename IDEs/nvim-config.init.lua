4297246826e8f865c7875878cc0302b183114e76
alexxmatei <49484290+alexxmatei@users.noreply.github.com>
2023-08-31 13:25:49 +0300

    Update nvim-config.init.lua

--- IDEs/nvim-config.init.lua
+++ IDEs/nvim-config.init.lua
@@ -1,3 +1,11 @@
 -- Set a vertical ruler as a recommended best practice not to cross over
 -- This makes it easier to work with multiple editors in split windows amongst other things
 vim.opt.colorcolumn = "80"
+
+-- PowerShell
+-- Set PowerShell as the default shell for running shell commands
+vim.o.shell = "powershell"
+-- Set the shell command flag for PowerShell
+vim.o.shellcmdflag = "-c"
+-- Set shellxquote to an empty string
+vim.opt.shellxquote = ''
