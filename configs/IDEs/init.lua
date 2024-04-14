6df1da611800a08629c27ea022f74bb102cca00b
alexxmatei <49484290+alexxmatei@users.noreply.github.com>
2024-04-14 15:57:38 +0300

    Update init.lua
    Add catppuccin theme to nvim

--- configs/IDEs/init.lua
+++ configs/IDEs/init.lua
@@ -134,12 +134,18 @@ require('lazy').setup({
     },
   },
 
+-- Themes
   {
     -- Theme inspired by Atom
     'navarasu/onedark.nvim',
     priority = 1000,
     config = function()
       vim.cmd.colorscheme 'onedark'
+    end,
+  },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000,
+    config = function()
+      vim.cmd.colorscheme 'catppuccin-latte'
     end,
   },
 
